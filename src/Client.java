a7ef9308ac5d2b1646334459e2e8f1858d959b81
Jacul <jaculing@gmail.com>
2011-12-10 18:37:53 -0500

    Now it's working. Server and clients can run on different hosts.

--- src/Client.java
+++ src/Client.java
@@ -1,25 +1,155 @@
+/**
+ * Xiangdong Zhu CSC 623 - Data communication and network
+ * 12/10/2011
+ * Fall 2011 Extra Credit Assignment #5 
+ */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
 
 /**
+ * Client to send messages. It connects to the server and exchanges messages.<br>
+ * When use it, you should either specify a client name or a client name with
+ * server address and port number.
  * 
  * @author zxd
  * 
  */
 public class Client extends Thread {
+	/**
+	 * Socket connection this client uses.
+	 */
+	private Socket socket = null;
+	/**
+	 * Name of this client.
+	 */
+	private String clientname = null;
+	/**
+	 * Server's address. By default it's local.
+	 */
+	private String serveraddress = "localhost";
+	/**
+	 * Port number of the server. By default it's 20000.
+	 */
+	private int serverport = 20000;
+	/**
+	 * To send message.
+	 */
+	private PrintWriter msgSender = null;
+	/**
+	 * Receive message.
+	 */
+	private BufferedReader msgReceiver = null;
 
+	/**
+	 * Constructor with client name only.
+	 * 
+	 * @param name
+	 */
 	public Client(String name) {
+		this.clientname = name;
+	}
 
+	/**
+	 * Constructor with client name and server url.
+	 * 
+	 * @param name
+	 * @param server
+	 */
+	public Client(String name, String server, int serverport) {
+		this.clientname = name;
+		this.serveraddress = server;
+		this.serverport = serverport;
+	}
+
+	public void run() {
+		connectToServer();
+		prepareMessageExchange();
+		Scanner userinput = new Scanner(System.in);
+		while (true) {
+			// read input from keyboard
+			String input = userinput.nextLine();
+			// send to server
+			msgSender.println(input);
+			msgSender.flush();
+			if (input.equalsIgnoreCase("logoff")) {
+				// user logs off, exit the program.
+				if (socket != null)
+					try {
+						socket.close();
+					} catch (IOException e) {
+						System.out.println(e.getClass().getName() + e.getMessage());
+					}
+				return;
+			} else if (input.toLowerCase().startsWith("send ")) {
+				System.out.print(">");
+			}
+		}
+	}
+
+	/**
+	 * Start a Thread to read responses and send an online notification to
+	 * server.
+	 */
+	private void prepareMessageExchange() {
+		new Thread() {
+			public void run() {
+				while (true) {
+					try {
+						String msg = msgReceiver.readLine();
+						if (msg == null) {
+							// server not available
+							System.out.println("Server not reachable! Quit.");
+							System.exit(0);
+						}
+						System.out.println(msg);
+
+					} catch (Exception e) {
+						System.exit(0);
+					}
+				}
+			}
+		}.start();
+		msgSender.println("Client " + clientname + " online");
+		msgSender.flush();
+	}
+
+	private void connectToServer() {
+		try {
+			socket = new Socket(serveraddress, serverport);
+			msgSender = new PrintWriter(socket.getOutputStream());
+			msgReceiver = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+		} catch (UnknownHostException e) {
+			// Server not available
+			System.out.println("Can't find server!");
+			System.exit(0);
+		} catch (IOException e) {
+			System.out.println(e.getClass().getName() + e.getMessage());
+		}
 	}
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		if (args != null && args.length > 0) {
+		if (args != null && args.length == 1) {
+			// user provides server name only.
 			new Client(args[0]).start();
+		} else if (args != null && args.length == 3) {
+			try {
+				int port = Integer.parseInt(args[2]);
+				new Client(args[0], args[1], port).start();
+			} catch (Exception e) {
+				System.out.println("Please specify a valid server port number!");
+				System.exit(0);
+			}
 		} else {
-			System.out.println("Specify a client name!");
+			System.out.println("Specify a client name [server address and port number]!");
 			System.exit(0);
 		}
 	}
-
 }
--- src/MEPServer.java
+++ src/MEPServer.java
@@ -1,30 +1,40 @@
+/**
+ * Xiangdong Zhu CSC 623 - Data communication and network
+ *  12/10/2011
+ * Fall 2011 Extra Credit Assignment #5 
+ */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 /**
- * Xiangdong Zhu CSC 623 - Data communication and network
+ * This class is the server. It can run on local server, listening for incoming
+ * connections. Its default port number is 20000, you can specify a port number
+ * just by appending a argument when starting this server in java command line.
  * 
  * @author zxd
  * 
  */
 public class MEPServer extends Thread {
 	/**
-	 * Containing all client information, ip address and port number.<br>
-	 * The key is the name of the client, and the value should be in this
-	 * format, IP:PORT, for example, localhost:20010.
+	 * Containing all clients that are online. Every client has a thread running
+	 * that is listening for its incoming messages.
 	 */
-	static final HashMap<String, String>	DNSTable	= new HashMap<String, String>();
-
-	static {
-		DNSTable.put("doc", "localhost:20010");
-		DNSTable.put("grumpy", "localhost:20011");
-		DNSTable.put("sleepy", "localhost:20012");
-		DNSTable.put("sneezy", "localhost:20013");
-		DNSTable.put("dopey", "localhost:20014");
-		DNSTable.put("bashful", "localhost:20015");
-		DNSTable.put("happy", "localhost:20016");
-	}
-	
-	private int								port;
+	private HashMap<String, Socket> availableClient = new HashMap<String, Socket>();
+	/**
+	 * Containing all clients that are not handled.
+	 */
+	private static ArrayList<Socket> unhandledRequest = new ArrayList<Socket>();
+	/**
+	 * Port number at which the server is listening.
+	 */
+	private int port;
 
 	/**
 	 * Constructor, with the port number.
@@ -35,8 +45,137 @@ public class MEPServer extends Thread {
 		this.port = port;
 	}
 
-	public void run() {
+	/**
+	 * Start the listener of this server. Also, it accepts every incoming
+	 * connection and processes it.
+	 */
+	public void startListening() {
+		System.out.println("Server started");
+		ServerSocket server = null;
+		try {
+			server = new ServerSocket(port);
+			while (true) {
+				Socket client = server.accept();
+				System.out.println("Client connected");
+				unhandledRequest.add(client);
+				new Thread(this).start();
+			}
+		} catch (IOException e) {
+			System.err.println("4: " + e.getClass().getName() + e.getMessage());
+		} finally {
+			if (server != null)
+				try {
+					server.close();
+				} catch (IOException e) {
+					System.err.println("3: " + e.getClass().getName() + e.getMessage());
+				}
+		}
 
+	}
+
+	public void run() {
+		Socket client = unhandledRequest.remove(0);
+		if (client == null)
+			return;
+		BufferedReader reader = null;
+		PrintWriter sender = null;
+		String clientname = null;
+
+		try {
+			reader = new BufferedReader(new InputStreamReader(client.getInputStream()));
+			sender = new PrintWriter(client.getOutputStream());
+			clientname = welcomeClient(client, reader, sender);
+			if (clientname == null) {
+				return;
+			}
+		} catch (IOException e) {
+			return;
+		}
+		while (true) {
+			try {
+				String msg = reader.readLine();
+				if (msg == null) {
+					// the client may have closed the connection, thread quits.
+					client.close();
+					return;
+				}
+				handleMessage(clientname, msg, reader, sender);
+			} catch (SocketException e) {
+				try {
+					client.close();
+					return;
+				} catch (IOException e1) {
+				}
+			} catch (IOException e) {
+				System.out.println("Reading message error: " + e.getClass().getName() + e.getMessage());
+			}
+		}
+
+	}
+
+	/**
+	 * Send a welcome message to the client.
+	 * 
+	 * @param client
+	 * @param reader
+	 * @param sender
+	 * @return
+	 * @throws IOException
+	 */
+	private String welcomeClient(Socket client, BufferedReader reader, PrintWriter sender) throws IOException {
+		String clientname = null;
+		String status = reader.readLine();
+		System.out.println("Incoming: " + status);
+
+		if (status.toLowerCase().startsWith("client ") && status.toLowerCase().endsWith(" online")) {
+			clientname = status.substring(status.indexOf(' ') + 1, status.lastIndexOf(' '));
+			availableClient.put(clientname, client);
+			sender.println("#Welcome! " + clientname);
+			sender.flush();
+		} else {
+			// Not a valid login message, close the connection.
+			client.close();
+		}
+		return clientname;
+	}
+
+	/**
+	 * Handles the message.
+	 * 
+	 * @param clientname
+	 * @param msg
+	 * @param reader
+	 * @param sender
+	 * @throws IOException
+	 */
+	private void handleMessage(String clientname, String msg, BufferedReader reader, PrintWriter sender) throws IOException {
+		if (msg.equalsIgnoreCase("logoff")) {
+			// client exits
+			availableClient.remove(clientname);
+		} else if (msg.startsWith("send ")) {
+			String dstclient = msg.substring(5);
+			if (!availableClient.containsKey(dstclient)) {
+				sender.println("#Client " + dstclient + " Not Available!");
+				sender.flush();
+				return;
+			}
+			String text = reader.readLine();
+			if (text != null) {
+				Socket dstclientsocket = availableClient.get(dstclient);
+				if (dstclientsocket.isClosed()) {
+					availableClient.remove(dstclient);
+					sender.println("#Client " + dstclient + " Not Available!");
+					sender.flush();
+					return;
+				}
+				PrintWriter msgsender = new PrintWriter(dstclientsocket.getOutputStream());
+				msgsender.println(clientname + ">" + text);
+				msgsender.flush();
+			}
+		} else {
+			sender.println("#Unrecognized message!");
+			sender.flush();
+		}
 	}
 
 	/**
@@ -45,12 +184,13 @@ public class MEPServer extends Thread {
 	public static void main(String[] args) {
 		int defaultport = 20000;
 		if (args != null && args.length > 0) {
+			// User specifies a port number.
 			try {
 				defaultport = Integer.parseInt(args[0]);
-			} catch (Exception e) {}
+			} catch (Exception e) {
+			}
 		}
-		MEPServer server = new MEPServer(defaultport);
-		server.start();
+		new MEPServer(defaultport).startListening();
 	}
 
 }
